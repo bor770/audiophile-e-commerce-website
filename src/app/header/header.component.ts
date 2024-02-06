91f59faf7075eba98a69275229f45b77cae3f385
Baruch Giverts <bor770@gmail.com>
2024-02-06 14:21:35 +0200

    mobile header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,20 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
+import { LetDirective } from '@ngrx/component';
+
+import { BaseComponent } from '../shared/base/base.component';
+
 @Component({
+  imports: [NgClass, NgIf, LetDirective],
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  styleUrls: [
+    './styles/header.component.css',
+    `./styles/mobile.header.component.css`,
+    `./styles/tablet.header.component.css`,
+    `./styles/desktop.header.component.css`,
+  ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent extends BaseComponent {}
